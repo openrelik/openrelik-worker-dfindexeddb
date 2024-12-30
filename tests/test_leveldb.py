5616614dc59fbd621b083b3ef4022c47c5037372
Syd Pleno <syd.pleno@gmail.com>
2024-12-30 16:43:55 +1100

    Update leveldb unit test

--- tests/test_leveldb.py
+++ tests/test_leveldb.py
@@ -15,6 +15,7 @@
 """Unit tests for OpenRelik leveldb task."""
 import os
 import pytest
+import tempfile
 import unittest
 from unittest import mock
 
@@ -50,6 +51,47 @@ class LevelDBTest(unittest.TestCase):
         with self.assertRaisesRegex(RuntimeError, 'No supported files'):
             result.get()
         self.assertEqual(result.status, 'FAILURE')
+
+    @mock.patch("subprocess.Popen")
+    @mock.patch("celery.result")
+    def test_input_files(self, mock_result, mock_popen):
+        """Tests a single input file."""
+        pipe_result = None
+        input_files = [
+            {
+                'id': 1,
+                'uuid': '6b5856b3ddf3463aa74197dffbc88f95',
+                'display_name': '000005.ldb',
+                'extension': 'ldb',
+                'data_type': 'file:generic',
+                'path': './test_data/leveldb/000005.ldb'
+            }
+        ]
+        workflow_id = 'fake_workflow_id'
+        task_config = {
+            'record_type': 'blocks',
+            'output_format': 'JSON'
+        }
+
+        with tempfile.TemporaryDirectory() as output_path:
+            result = command.s(
+                pipe_result=pipe_result,
+                input_files=input_files,
+                output_path=output_path,
+                workflow_id=workflow_id,
+                task_config=task_config
+            ).apply()
+
+            _ = result.get()
+        mock_popen.assert_called_once_with(
+            [
+                'dfleveldb', 'ldb',
+                '-s', './test_data/leveldb/000005.ldb',
+                '-t', 'blocks',
+                '-o', 'json'
+            ],
+            stdout=mock.ANY, stderr=mock.ANY
+        )
 
 
 if __name__ == '__main__':
